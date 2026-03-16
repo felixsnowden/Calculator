40fc72f998323711ed533c8584607a68258852c0
Yurii Smirnov <yurasmirnov0@gmail.com>
2026-03-16 16:49:54 +0100

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+double a;
+double b;
+double c;
+
+Console.Write("Write first number: ");
+a = double.Parse(Console.ReadLine());
+Console.Write("Write second number: ");
+b = double.Parse(Console.ReadLine());
+
+Console.Write("Write operator: ");
+char operat = char.Parse(Console.ReadLine());
+
+switch (operat)
+{
+    case '*':
+        c = a * b;
+        Console.WriteLine(c);
+        break;
+    case '+':
+        c = a + b;
+        Console.WriteLine(c);
+        break;
+    case '-':
+        c = a - b;
+        Console.WriteLine(c);
+        break;
+    case '/':
+        c = a / b;
+        Console.WriteLine(c);
+        break;
+}
